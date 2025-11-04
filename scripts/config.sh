31697bdcdc22c4bba4702b42e7189841a7f93609
Enmanuel Basulto Martínez <76921069+enmanuelbasulto@users.noreply.github.com>
2025-11-04 13:04:50 -0500

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -19,7 +19,7 @@ export TARGET_KERNEL_PACKAGE="linux-generic"
 
 # The file (no extension) of the ISO containing the generated disk image,
 # the volume id, and the hostname of the live environment are set from this name.
-export TARGET_NAME="ubuntu-pbx
+export TARGET_NAME="ubuntu-pbx"
 
 # The text label shown in GRUB for booting into the live environment
 export GRUB_LIVEBOOT_LABEL="Probar Ubuntu-PBX sin instalar"
