01a3cf7a5e96310bf5d22b912b15f64e624659fe
Enmanuel Basulto Martínez <76921069+enmanuelbasulto@users.noreply.github.com>
2025-11-08 15:55:25 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -55,8 +55,8 @@ function chroot_exit_teardown() {
     sudo chroot chroot umount /proc
     sudo chroot chroot umount /sys
     sudo chroot chroot umount /dev/pts
-    sudo umount chroot/dev
-    sudo umount chroot/run
+    sudo umount -l chroot/dev
+    sudo umount -l chroot/run
 }
 
 function check_host() {
